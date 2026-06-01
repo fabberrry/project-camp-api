d09cfdaae22af25708e97686a174964e7de66925
fabberrry <rhysget0@gmail.com>
2026-06-01 12:04:28 +0530

    feat: implement MongoDB connection logic in a separate module

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/index.js";
 dotenv.config({
   path: "./.env",
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
-});
-
-
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB:", err);
+    process.exit(1);
+  });
