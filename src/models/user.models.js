8dbfb8be0de0be073fcf0280b105d177d5950650
fabberrry <rhysget0@gmail.com>
2026-09-02 12:52:19 +0530

    feat: add JWT authentication methods to user model and update dependencies

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,8 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 const userSchema = new Schema(
   {
     avatar: {
@@ -69,6 +72,53 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAT = function () {
+  return jwt.sign(
+    {
+      _id: this._id, // payload
+      email: this.email, // payload
+      username: this.username, // payload
+    },
+    process.env.ACCESS_TOKEN_SECRET, // secret key
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRATION,
+    },
+  );
+};
+userSchema.methods.generateRT = function () {
+  return jwt.sign(
+    {
+      _id: this._id, // payload
+      email: this.email, // payload
+      username: this.username, // payload
+    },
+    process.env.REFRESH_TOKEN_SECRET, // secret key
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRATION,
+    },
+  );
+};
+
+// temperory token for email verification and forgot password
+userSchema.methods.generateTemperoryToken = function () {
+  const unhashedtoken = crypto.randomBytes(32).toString("hex");
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(unhashedtoken)
+    .digest("hex");
+  // where sha256 is the hashing algorithm,
+  // update() is used to update the hash with the unhashed token, and digest() is used to get the final hashed value in hexadecimal format.
+
+  //token expiry
+  const tokenExpiry = Date.now() + 10 * 60 * 1000; // 10 minutes from now
+  // return all the values
+  return {
+    unhashedtoken,
+    hashedToken,
+    tokenExpiry,
+  };
 };
 
 export const User = mongoose.model("User", userSchema);
