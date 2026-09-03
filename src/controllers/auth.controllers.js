d0750a07290ab7795ceb5d432e4b0ac8f49d512d
fabberrry <rhysget0@gmail.com>
2026-09-04 00:58:02 +0530

    fix(auth): correct email verification link in registration process
    - Updated the email verification link in the registerUser function to use the correct route for email verification.
    - This change ensures that users receive the proper link to verify their email after registration.

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -63,7 +63,7 @@ const registerUser = asyncHandler(async (req, res) => {
     subject: "Verify your email",
     mailgencontent: emailVerficationMailGeneratorContent(
       user.username,
-      `${req.protocol}://${req.get("host")}/api/v1/users/verify-email/${unhashedtoken}`,
+      `${req.protocol}://${req.get("host")}/api/v1/auth/verify-email/${unhashedtoken}`,
     ),
   });
 
