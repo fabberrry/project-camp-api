4abc61911f99566920792da1d70406d862764a08
fabberrry <rhysget0@gmail.com>
2026-09-04 21:42:01 +0530

    feat(auth): add JWT authentication middleware  - extract access token from cookies or Authorization header  - verify access token using the configured JWT secret  - fetch the authenticated user while excluding sensitive fields  - attach the authenticated user to the request object  - reject missing or invalid access tokens with unauthorized errors

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/asyncHandler.js|";
+import jwt from "jsonwebtoken";
+
+export const verifyJwt = asyncHandler(async (req, res, next) => {
+  const tokenAT =
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
+  if (!tokenAT) {
+    throw new ApiError(401, "Unauthorized Request");
+  }
+
+  try {
+    const decodedToken = jwt.verify(tokenAT, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findOne(decodedToken?._id).select(
+      "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+    );
+    if (!user) {
+      throw new ApiError(401, "Invalid AccessToken");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, "Invalid AccessToken");
+  }
+});
