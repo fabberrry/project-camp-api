45c24acb09e91ca5feb40d25cc3931bbdbeaedb2
fabberrry <rhysget0@gmail.com>
2026-09-04 18:26:53 +0530

    feat: add user registration route with validation

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,6 +3,14 @@ import {Router} from "express";
 const router = Router();
 
 import {registerUser} from "../controllers/auth.controllers.js";
-router.route("/register").post(registerUser);
+// router.route("/register").post(registerUser); 
+
+
+
+import { userRegisterValiator } from "../validators/index.js";
+import { validateRequest } from "../middlewares/validator.middleware.js";
+router.route("/register").post(userRegisterValiator(), validateRequest, registerUser);
+
+
 
 export default router;
