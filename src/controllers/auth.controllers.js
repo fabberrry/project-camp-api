1614828741f41053895e206f5b57554247589f9a
fabberrry <rhysget0@gmail.com>
2026-09-05 10:22:04 +0530

    feat(auth): implement change current password  - require old password

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -329,6 +329,21 @@ const resetForgotPassword = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, {}, "Password reset successfully"));
 });
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  const user = await User.findById(req.user?._id);
+
+  const isPasswordValid = await user.isPasswordCorrect(oldPassword);
+  if (!isPasswordValid) {
+    throw new ApiError(400, "Invalid Old Password");
+  }
+
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"));
+});
 // const verifyEmail=asyncHandler(async(req,res)=>{})
 export {
   registerUser,
@@ -339,5 +354,6 @@ export {
   resendEmailVerification,
   refreshAccessToken,
   forgotPasswordRequest,
-  resetForgotPassword
+  resetForgotPassword,
+  changeCurrentPassword
 };
