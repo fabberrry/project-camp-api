553bf54350c1a9c480d510d1efa61fd17e120345
fabberrry <rhysget0@gmail.com>
2026-09-08 00:19:45 +0530

    chore: add cookie-parser middleware for cookie handling

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 const app = express();
-
+app.use(cookieParser());
 // basic configurations
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
