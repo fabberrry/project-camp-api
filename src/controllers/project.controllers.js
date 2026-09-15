eb63df24f1b987b058f26e741c954f0e1f7234da
fabberrry <rhysget0@gmail.com>
2026-09-15 11:14:51 +0530

    feat(projects): add user project retrieval with aggregation
    - fetch projects associated with the authenticated user
    - join project details using MongoDB aggregation
    - retrieve project members and calculate member count
    - include the user's role for each project
    - return structured project details in the API response

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -13,6 +13,59 @@ import { UserRolesEnum } from "../utils/constants.js";
 
 const getProjectsOfUser = asyncHandler(async (req, res) => {
   //test
+  const projects = await projectMember.aggregate([
+    {
+      $match: {
+        user: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "projects",
+        localField: "projects",
+        foreignField: "_id",
+        as: "projects",
+        pipeline: [
+          {
+            $lookup: {
+              from: "projectmembers",
+              localField: "_id",
+              foreignField: "projects",
+              as: "projectmembers",
+            },
+          },
+          {
+            $addFields: {
+              members: {
+                $size: "$projectmembers",
+              },
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: "$project",
+    },
+    {
+      $project: {
+        project: {
+          _id: 1,
+          name: 1,
+          description: 1,
+          members: 1,
+          createdAt: 1,
+          createdBy: 1,
+        },
+        role: 1,
+        _id: 0,
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Project fetched successfully", projects));
 });
 const getProjectId = asyncHandler(async (req, res) => {
   //test
